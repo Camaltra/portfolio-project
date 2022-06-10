8eac186c24bcac9d6068d6ceedf90c63ef53f669
Camaltra <mickael.boillaud@gmail.com>
2022-06-10 19:06:39 +0200

    Adding the login page FRONT

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Router, Route, Navigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import LoginPage from "./routes/login/login.component";
+
+const Info = () => {
+  return <h1>This is some information about the project</h1>;
+};
+
 function App() {
-  return <h1>Starting</h1>;
+  return (
+    <Routes>
+      <Route index element={<LoginPage />} />
+      <Route path="/info" element={<Info />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/routes/login/login.component.jsx
@@ -0,0 +1,35 @@
+import "./login.style.scss";
+
+const LoginPage = () => {
+  const loginWithGoogle = () => {
+    window.open("http://localhost:8000/auth/google", "_self");
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-header">
+        <h1>Hyppo'nterview</h1>
+      </div>
+      <div className="login-card">
+        <h1>Welcome Mate</h1>
+        <h2>Please Log-In/Sign-Up</h2>
+        <div className="login-google-button" onClick={loginWithGoogle}>
+          <p>Connect With Google</p>
+        </div>
+        <p
+          className="get-info-button"
+          onClick={() => {
+            window.open("/info", "_self");
+          }}
+        >
+          Get more information about the project
+        </p>
+      </div>
+      <div className="login-footer">
+        <h3>Portfolio Project made by Mickael Boillaud</h3>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
