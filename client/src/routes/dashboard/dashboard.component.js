defb4abb62a15b777b23111b0f964d8eef713740
Camaltra <mickael.boillaud@gmail.com>
2022-06-10 23:39:30 +0200

    Adding a little structure of the futur task component

--- client/src/routes/dashboard/dashboard.component.js
+++ client/src/routes/dashboard/dashboard.component.js
@@ -1,4 +1,5 @@
 import { useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 import UserProvider from "../../context/user/user.context";
 
@@ -6,11 +7,19 @@ const Dashboard = () => {
   const user = useContext(UserProvider.context);
 
   const [isLoading, setLoading] = useState(true);
+  const [allTasks, setAllTasks] = useState([]);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 100);
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/v1/tasks")
+      .then((response) => setAllTasks(response.data))
+      .catch((err) => console.log(err));
   }, []);
 
   return isLoading ? (
@@ -23,6 +32,14 @@ const Dashboard = () => {
       <div>
         <h1>THIS IS THE DASHBOARD</h1>
         <p>{user}</p>
+        {allTasks.map((task) => {
+          return (
+            <div key={task.id}>
+              <h1>{task.id}</h1>
+              <p>{task.description}</p>
+            </div>
+          );
+        })}
       </div>
     </>
   );
