bb3dd1b262e23d46225fd009b261645975f1ca78
Camaltra <mickael.boillaud@gmail.com>
2022-06-30 10:02:03 +0200

    Adding 2 new tasks

---
+++ server-checker/src/checker/0x00-array_string/4-valid_parentheses/0-main.py
@@ -0,0 +1,22 @@
+
+def validParenthesesChecker(parenthesesString):
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in parenthesesString:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return True
+
+parenthesesString = "()"
+
+try:
+    if (validParenthesesChecker(parenthesesString) == validParentheses(parenthesesString)):
+        print('OK')
+    else:
+        print("NotOK")
+except Exception:
+    print('NotOK')
