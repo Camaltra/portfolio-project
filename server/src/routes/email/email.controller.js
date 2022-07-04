6cf3745247052a49de308e566e64710e5f7fc8a8
Camaltra <mickael.boillaud@gmail.com>
2022-07-04 09:25:27 +0200

    Adding a security with a catch, and a statement if any params are in the .env

--- server/src/routes/email/email.controller.js
+++ server/src/routes/email/email.controller.js
@@ -1,5 +1,7 @@
 const sendSignInEmail = require("../../services/email");
 
+require("dotenv").config();
+
 const httpSendSignUpEmail = async (req, res) => {
   const email = req.body.email;
   const username = req.body.username;
@@ -8,7 +10,16 @@ const httpSendSignUpEmail = async (req, res) => {
     return res.status(400).json({ error: "Can't send the email" });
   }
 
-  await sendSignInEmail(email, username);
+  if (
+    !process.env.SENDGRID_API_KEY ||
+    !process.env.SENDGRID_EMAIL ||
+    !process.env.SENDGRID_SIGNUP_TEMPLATE
+  ) {
+    return res.status(200).json({ error: "Params not initilize" });
+  }
+  await sendSignInEmail(email, username).catch((err) => {
+    console.error(err);
+  });
 
   return res.status(200).json({ success: "Email send" });
 };
